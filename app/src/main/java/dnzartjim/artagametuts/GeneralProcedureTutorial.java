ae75403cc6651d776664713354c85b105590f756
a_jimwel <anobongjimwel@outlook.ph>
2019-02-16 17:14:51 +0800

    Small updates
    - Change the colors of the app
    - Done making the layout for the third procedure in general procedures tutorial activity

--- app/src/main/java/dnzartjim/artagametuts/GeneralProcedureTutorial.java
+++ app/src/main/java/dnzartjim/artagametuts/GeneralProcedureTutorial.java
@@ -13,18 +13,27 @@ public class GeneralProcedureTutorial extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_general_procedure_tutorial);
+        MediaController mcVV = new MediaController(this);
 
         VideoView vvPreparingCables = findViewById(R.id.vvPreparingCables);
-        MediaController mcVV = new MediaController(this);
         Uri uriPreparingCables = Uri.parse("android.resource://" + getPackageName() + "/raw/preparecable");
         vvPreparingCables.setVideoURI(uriPreparingCables);
         vvPreparingCables.setMediaController(mcVV);
-        vvPreparingCables.setOnPreparedListener(new MediaPlayer.OnPreparedListener() {
+        mutePlayerAtFirst(vvPreparingCables);
+
+        VideoView vvCutFirstSide = findViewById(R.id.vvCutFirstSide);
+        Uri uriCutFirstSide = Uri.parse("android.resource://" + getPackageName() + "/raw/cutfirstside");
+        vvCutFirstSide.setVideoURI(uriCutFirstSide);
+        vvCutFirstSide.setMediaController(mcVV);
+        mutePlayerAtFirst(vvCutFirstSide);
+    }
+
+    private void mutePlayerAtFirst(VideoView videoview) {
+        videoview.setOnPreparedListener(new MediaPlayer.OnPreparedListener() {
             @Override
             public void onPrepared(MediaPlayer mp) {
                 mp.setVolume(0f, 0f);
             }
         });
-        vvPreparingCables.start();
     }
 }
